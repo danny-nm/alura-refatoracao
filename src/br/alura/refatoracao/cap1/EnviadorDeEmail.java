627cef7b0ac35177bb16cd9a68bac3436c1dd216
Daniella Nunes Mendes <danny.nm11@gmail.com>
2016-04-04 12:51:36 -0300

    commit teste

--- src/br/alura/refatoracao/cap1/EnviadorDeEmail.java
+++ src/br/alura/refatoracao/cap1/EnviadorDeEmail.java
@@ -7,7 +7,7 @@ public class EnviadorDeEmail {
 		String msgDoEmail = "Caro cliente,<br/>";
 		msgDoEmail += "É com prazer que lhe avisamos que sua nota fiscal foi "
 				+ "gerada no valor de " + nf.getValorLiquido() + ".<br/>";
-		msgDoEmail += "Acesse o site da prefeitura e faÃ§a o download.<br/><br/>";
+		msgDoEmail += "Acesse o site da prefeitura e faça o download.<br/><br/>";
 		msgDoEmail += "Obrigado!";
 		
 		System.out.println(msgDoEmail);
